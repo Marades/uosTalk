cfb59f02b00af8f856e83d9e2ff522256dc2af6f
Marades <github.com/Marades>
2018-06-01 00:05:29 +0900

    first commit

---
+++ routes/v1/petition.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const sql = require('sql-loader')
+const router = express.Router()
+
+//전체 청원 가져오기
+router.get('/', (req, res) => {
+    pool.query(sql.petition.select, (err, rows) => {
+        if(err) {
+            console.log(err)
+            res.json({
+                msg: 'error occur',
+                err: err
+            })
+        }
+        else {
+            res.json({
+                msg: 'success',
+                data: rows
+            })
+        }
+    })
+})
+
+// 청원 작성
+router.post('/', (req, res) => {
+    console.log(req.body)
+    const insert_data = {
+        title: req.body.title,
+        content: req.body.content
+    }
+    pool.query(sql.petition.insert, insert_data, (err, rows) => {
+        if(err) {
+            console.log(err)
+            res.json({
+                msg: 'error occur',
+                err: err
+            })
+        }
+        else {
+            console.log(insert_data)
+            res.json({
+                msg: 'success',
+            })
+        }
+    })
+})
+
+module.exports = router
